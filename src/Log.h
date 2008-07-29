ffde601ca893202f4a5c914a4f229e0683ea1cf2
Per Øyvind Karlsen <peroyvind@localhost>
2008-07-29 17:12:52 +0200

    add dummy logging..

---
+++ src/Log.h
@@ -0,0 +1,11 @@
+#ifndef LOG_H
+#define LOG_H
+#define LOG(...)           ((void)(0))
+#define LOG_FATAL(...)     ((void)(0))
+#define LOG_ERROR(...)     ((void)(0))
+#define LOG_WARNING(...)   ((void)(0))
+#define LOG_INFO(...)      ((void)(0))
+
+#define LOG_INDENT(...)      ((void)(0))
+#define LOG_UNINDENT(...)    ((void)(0))
+#endif // LOG_H
