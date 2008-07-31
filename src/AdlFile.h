3a192c744bfcf36cf63e18a2cbf2bfc35308c3fc
Per Øyvind Karlsen <peroyvind@localhost>
2008-07-31 03:28:20 +0200

    move around things to make things work proerly,. blablala

--- src/AdlFile.h
+++ src/AdlFile.h
@@ -1,27 +1,87 @@
-#ifndef EASTWOOD_ADLFILE_H
-#define EASTWOOD_ADLFILE_H
+/*
+ * Adplug - Replayer for many OPL2/OPL3 audio file formats.
+ * Copyright (C) 1999 - 2006 Simon Peter, <dn.tlp@gmx.net>, et al.
+ * 
+ * adl.h - ADL player adaption by Simon Peter <dn.tlp@gmx.net>
+ */
 
-class CadlPlayer;
+#ifndef EASTWOOD_ADLIBFILE_H
+#define EASTWOOD_ADLIBFILE_H 
+
+#include <string>
+#include <inttypes.h>
+
+
+#include <SDL.h>
+#include <SDL_mixer.h>
+
+class AdlibDriver;
 class Copl;
-class AdlFile
+class CadlPlayer
 {
-	public:
-		AdlFile();
-		~AdlFile();
-		static void callback(void *userdata, Uint8 *audiobuf, int len);
-		int m_channels;
-		int m_freq;
-		Uint16 m_format;
-		bool playing;
-	      	bool load(uint8_t *bufFiledata, int bufsize);
-	      	void rewind(int subsong);
-	
-	private:
-		Copl *m_opl;
-		CadlPlayer *m_adlPlayer;
-		unsigned char getsampsize() { 
-			return m_channels * (m_format == AUDIO_U8 ? 1 : 2); }
+ public:
+  CadlPlayer(Copl *newopl);
+  CadlPlayer();
+  ~CadlPlayer();
+
+  static void callback(void *, Uint8 *, int);
+  bool load(uint8_t *bufFiledata, int bufsize);
+  bool update();
+  void rewind(int subsong);
+  Copl* get_opl() { return opl; }
+  bool init();
+  // refresh rate is fixed at 72Hz
+  float getrefresh()
+    {
+      return 72.0f;
+    }
+
+  unsigned int getsubsongs();
+  std::string gettype() { return std::string("Westwood ADL"); }
+
+ private:
+  int numsubsongs, cursubsong;
+
+  AdlibDriver *_driver;
+  Copl* opl;
+  
+  uint8_t _trackEntries[120];
+  uint8_t *_soundDataPtr;
+  int _sfxPlayingSound;
+
+  uint8_t _sfxPriority;
+  uint8_t _sfxFourthByteOfSong;
+
+  int _numSoundTriggers;
+  const int *_soundTriggers;
+  
+  int m_channels;
+  int m_freq;
+  Uint16 m_format;
+  
+  bool playing; //FIXME: It could be used for something, I hope.
+
+  static const int _kyra1NumSoundTriggers;
+  static const int _kyra1SoundTriggers[];
+
+
+  
+  unsigned char getsampsize() { 
+        return m_channels * (m_format == AUDIO_U8 ? 1 : 2); }
+        
+  void process();
+  void playTrack(uint8_t track);
+  void playSoundEffect(uint8_t track);
+  void play(uint8_t track);
+  void unk1();
+  void unk2();
 
 };
 
-#endif // EASTWOOD_ADLFILE_H
+class AdlFile : public CadlPlayer
+{
+ public:
+  AdlFile();
+};
+
+#endif // EASTWOOD_ADLIBFILE_H
