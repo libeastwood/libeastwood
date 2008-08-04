f9adc365d418c67d6809789cb3416a6d7abe4127
Per Øyvind Karlsen <peroyvind@localhost>
2008-08-04 12:57:27 +0200

    refactorize

--- src/PalFile.cpp
+++ src/PalFile.cpp
@@ -2,7 +2,7 @@
 
 #include "PalFile.h"
 
-PalFile::PalFile(unsigned char *buf, int bufSize)
+PalFile::PalFile(unsigned char *bufFileData, int bufSize)
 {
     LOG_INFO("PalFile", "Loading palette...");
     
@@ -10,17 +10,14 @@ PalFile::PalFile(unsigned char *buf, int bufSize)
     m_palette->ncolors = bufSize / 3;
 
     m_palette->colors = new SDL_Color[m_palette->ncolors];
-    SDL_Color *c = m_palette->colors;
-    unsigned char *dunePal = buf;
-    
-    for (int i=0; i!=m_palette->ncolors; i++, c++, dunePal+=3)
-    {
-        c->r = (dunePal[0] << 2) + 3;
-        c->g = (dunePal[1] << 2) + 3;
-        c->b = (dunePal[2] << 2) + 3;
-    };
 
-    delete [] buf;
+    for (int i = 0; i < m_palette->ncolors; i++){
+	    m_palette->colors[i].r = *bufFileData++ <<2;
+	    m_palette->colors[i].g = *bufFileData++ <<2;
+	    m_palette->colors[i].b = *bufFileData++ <<2;
+    }
+
+    delete [] bufFileData;
 }
 
 PalFile::~PalFile()
