9aa7ef7496fcbeb11d099b0059b871588d782a69
Per Øyvind Karlsen <proyvind@gmail.com>
2009-01-31 18:58:01 +0100

    be sure to initialize m_palette->colors[i].unused so that we can use allocate m_palette->colors as intended without any errors:)

--- src/PalFile.cpp
+++ src/PalFile.cpp
@@ -9,19 +9,18 @@ PalFile::PalFile(unsigned char *bufFileData, int bufSize)
     m_palette = new SDL_Palette;
     m_palette->ncolors = bufSize / 3;
 
-    // FIXME: Why won't this properly initialize?
-    // m_palette->colors = new SDL_Color[m_palette->ncolors];
-    m_palette->colors = (SDL_Color*)calloc(m_palette->ncolors, sizeof(SDL_Color));
+    m_palette->colors = new SDL_Color[m_palette->ncolors];
 
     for (int i = 0; i < m_palette->ncolors; i++){
-	    m_palette->colors[i].r = *bufFileData++ <<2;
-	    m_palette->colors[i].g = *bufFileData++ <<2;
-	    m_palette->colors[i].b = *bufFileData++ <<2;
+	m_palette->colors[i].r = *bufFileData++ <<2;
+	m_palette->colors[i].g = *bufFileData++ <<2;
+	m_palette->colors[i].b = *bufFileData++ <<2;
+	m_palette->colors[i].unused = 0;
     }
 }
 
 PalFile::~PalFile()
 {
-    free(m_palette->colors);
+    delete[] m_palette->colors;
     delete m_palette;
 }
