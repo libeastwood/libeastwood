219a1968928f58c75a01e9f625ae2f749d897c6b
Per Øyvind Karlsen <proyvind@gmail.com>
2009-07-16 16:42:50 +0200

    really make it portable

--- src/StdDef.h
+++ src/StdDef.h
@@ -8,12 +8,25 @@
 
 #ifdef _WIN32
 #include <tchar.h>
-#define htobe16(x) htons(x)
-#define htobe32(x) htonl(x)
-#define htonl16(x) (x)
-#define htonl32(x) (x)
 #else
 #include <netinet/in.h>
 #endif
+#ifndef htobe16
+#define htobe16(x) htons(x)
+#define htobe32(x) htonl(x)
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#define htonl16(x) (x)
+#define htonl32(x) (x)
+#else
+static inline uint16_t htonl16(uint16_t x) {
+	return((x<<8)|(x>>8));
+}
+
+static inline uint32_t htonl32(uint32_t x) {
+	return((x<<24)|((x<<8)&0x00FF0000)|((x>>8)&0x0000FF00)|(x>>24));
+}
+#endif
+
+
 
 #endif // EASTWOOD_STDDEF_H
