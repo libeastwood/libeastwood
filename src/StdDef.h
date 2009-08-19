4920b3ef70b2af7c103609442498be466f27d566
Per Øyvind Karlsen <proyvind@gmail.com>
2009-08-19 18:52:02 +0200

    fix typo

--- src/StdDef.h
+++ src/StdDef.h
@@ -15,14 +15,14 @@
 #define htobe16(x) htons(x)
 #define htobe32(x) htonl(x)
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-#define htonl16(x) (x)
-#define htonl32(x) (x)
+#define htole16(x) (x)
+#define htole32(x) (x)
 #else
-static inline uint16_t htonl16(uint16_t x) {
+static inline uint16_t htole16(uint16_t x) {
 	return((x<<8)|(x>>8));
 }
 
-static inline uint32_t htonl32(uint32_t x) {
+static inline uint32_t htole32(uint32_t x) {
 	return((x<<24)|((x<<8)&0x00FF0000)|((x>>8)&0x0000FF00)|(x>>24));
 }
 #endif
