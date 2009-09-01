7314b5b56a41ed6623ee6ed143ee129132026a5c
Per Øyvind Karlsen <proyvind@gmail.com>
2009-09-01 13:19:50 +0200

    use all parts of CPS header

--- src/CpsFile.cpp
+++ src/CpsFile.cpp
@@ -12,12 +12,27 @@
 
 using namespace eastwood;
 
-CpsFile::CpsFile(std::istream &stream, SDL_Palette *palette) : Decode(stream, 320, 200, palette)
+CpsFile::CpsFile(std::istream &stream, SDL_Palette *palette) :
+    Decode(stream, 320, 200, palette), _format(UNCOMPRESSED), _imageSize(0)
 {
-    _stream.ignore(2);
-    if(readU16LE(_stream) != 0x0004 ||
-	    readU16LE(_stream) != 0xFA00)
-	throw(Exception(LOG_ERROR, "CpsFile", "Invalid header"));
+    if((uint16_t)(readU16LE(_stream)+_stream.gcount()) != getStreamSize(_stream))
+	throw(Exception(LOG_ERROR, "CpsFile", "Invalid file size"));
+
+    _format = (compressionFormat)readU16LE(_stream);
+    switch(_format) {
+	case UNCOMPRESSED:
+	case FORMAT_80:
+	    break;
+	case FORMAT_LBM:
+	    throw(Exception(LOG_ERROR, "CpsFile", "LBM format support not implemented"));
+	default:
+	    char error[256];
+	    snprintf(error, sizeof(error), "Format not supported: %x", _format);
+	    throw(Exception(LOG_ERROR, "CpsFile", error));
+    }
+
+    if((_imageSize = readU16LE(_stream)) != _width*_height)
+	throw(Exception(LOG_ERROR, "CpsFile", "Invalid image size"));
 
     _stream.ignore(2);
 
@@ -34,7 +49,7 @@ CpsFile::~CpsFile()
 
 SDL_Surface *CpsFile::getSurface()
 {
-    std::vector<uint8_t> ImageOut(_width*_height);
+    std::vector<uint8_t> ImageOut(_imageSize);
 
     if(decode80(&ImageOut.front(),0) == -2)
 	throw(Exception(LOG_ERROR, "CpsFile", "Cannot decode Cps-File"));
--- src/CpsFile.h
+++ src/CpsFile.h
@@ -6,13 +6,25 @@
 class SDL_Palette;
 class SDL_Surface;
 
+enum compressionFormat {
+    UNCOMPRESSED = 0x000,
+    FORMAT_LBM = 0x003,
+    FORMAT_80 = 0x004
+};
+
 class CpsFile : public Decode
 {
-public:
+    public:
 	CpsFile(std::istream &stream, SDL_Palette *palette = NULL);
 	virtual ~CpsFile();
 
 	SDL_Surface *getSurface();
+
+    private:
+	compressionFormat _format;
+	uint16_t _imageSize;
+
+
 };
 
 #endif // EASTWOOD_CPSFILE_H
