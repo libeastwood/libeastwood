87eb37220278f40eb0c7195ea298c8bbd6951cc1
Per Øyvind Karlsen <proyvind@gmail.com>
2009-09-05 01:05:41 +0200

    indent and some more cosmetics...

--- src/Font.cpp
+++ src/Font.cpp
@@ -14,56 +14,49 @@ Font::Font(FNTCharacter *characters, FNTHeader *header) :
 
 Font::~Font()
 {
-	delete [] _characters;
-	delete _header;
+    delete [] _characters;
+    delete _header;
 }
 
 void Font::extents(std::string text, uint16_t& w, uint16_t& h)
 {
-	FNTCharacter *ch;
-	w = 0;
-    	h = _header->height;
+    FNTCharacter *ch;
+    w = 0;
+    h = _header->height;
 
-    	for (unsigned int c=0; c!=text.length(); c++)
-    	{
-		ch = &_characters[(int)text[c]];
-		w += (2 * ch->width) + 1;
+    for (unsigned int c=0; c!=text.length(); c++)
+    {
+	ch = &_characters[(int)text[c]];
+	w += (2 * ch->width) + 1;
     };
 }
 
 void Font::render(std::string text, Surface *surface, int offx, int offy, uint8_t paloff)
 {
-	std::string test = text;
-	FNTCharacter *ch;
-	uint8_t *bitmap;
+    std::string test = text;
+    FNTCharacter *ch;
+    uint8_t *bitmap;
 
-	uint8_t* pixels = (uint8_t*)surface->_pixels;
+    uint8_t* pixels = (uint8_t*)surface->_pixels;
 
-    	for (unsigned int c=0; c!=text.length(); c++)
-	{
-		ch = &_characters[(int)text[c]];
-		bitmap = ch->bitmap;
+    for (unsigned int c=0; c!=text.length(); c++) {
+	ch = &_characters[(int)text[c]];
+	bitmap = ch->bitmap;
 
-		for (uint8_t y=0; y!=ch->height; y++)
-		{
-			for (uint8_t x=0; x!=ch->width*2; x+=2)
-	    		{
-				uint8_t lobyte = bitmap[(x/2) + (y*ch->width)] >> 4;
-				uint8_t hibyte = bitmap[(x/2) + (y*ch->width)] & 0x0F;
+	for (uint8_t y=0; y!=ch->height; y++) {
+	    for (uint8_t x=0; x!=ch->width*2; x+=2) {
+		uint8_t lobyte = bitmap[(x/2) + (y*ch->width)] >> 4;
+		uint8_t hibyte = bitmap[(x/2) + (y*ch->width)] & 0x0F;
 
-				if (hibyte!=0)
-				{
-					pixels[(offx + x) + ((ch->y_offset + y + offy) * surface->_width)] = paloff + uint8_t(hibyte);
-				};
+		if (hibyte!=0)
+		    pixels[(offx + x) + ((ch->y_offset + y + offy) * surface->_width)] = paloff + uint8_t(hibyte);
 
-				if (lobyte!=0) //(2 < ch->width) lobyte!=0)
-				{
-					pixels[(offx + x + 1) + ((ch->y_offset + y + offy) * surface->_width)] = paloff + uint8_t(lobyte);
-				};
-			};
-		};
-	offx += (2*ch->width) + 1;
+		if (lobyte!=0) //(2 < ch->width) lobyte!=0)
+		    pixels[(offx + x + 1) + ((ch->y_offset + y + offy) * surface->_width)] = paloff + uint8_t(lobyte);
+	    };
 	};
+	offx += (2*ch->width) + 1;
+    };
 }
 
 }
