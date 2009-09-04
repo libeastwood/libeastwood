c6b4a4591b13c850a65cc2701c745c73fee3ef85
Per Øyvind Karlsen <proyvind@gmail.com>
2009-09-05 00:49:19 +0200

    implement read() & seek() for python

--- src/python/pakfile.cpp
+++ src/python/pakfile.cpp
@@ -1,28 +1,46 @@
+// TODO: This is *very* crude for now, need to figure out some neat way for
+// 	 python to deal with C++ streams ...
 #include <istream>
 #include <fstream>
 #include <sstream>
 #include "pyeastwood.h"
+
 #include "PakFile.h"
+#include "StdDef.h"
 
 using namespace eastwood;
+
+enum file_mode {
+	MODE_CLOSED = 0,
+	MODE_READ = 1,
+	MODE_READ_EOF = 2
+};
 
 struct Py_PakFile {
 	PyObject_HEAD
 	std::ifstream *stream;
 	PakFile *pakFile;
+	uint32_t fileSize;
+	file_mode mode;
 };
 
 static int
 PakFile_init(Py_PakFile *self, PyObject *args)
 {
-    size_t fileSize;
+    Py_ssize_t size;
     char *fileName = NULL;
-    if (!PyArg_ParseTuple(args, "s#", &fileName, &fileSize))
+    if (!PyArg_ParseTuple(args, "s#", &fileName, &size))
 	return -1;
 
-    self->stream = new std::ifstream(fileName);
+    self->stream = new std::ifstream(fileName, std::ios::in | std::ios::binary);
+    if(!self->stream->good())
+    {
+	PyErr_SetFromErrno(PyExc_IOError);
+    	return -1;
+    }
 
     self->pakFile = new PakFile(*self->stream);
+    self->mode = MODE_CLOSED;
 
     return 0;
 }
@@ -47,20 +65,92 @@ PakFile_listfiles(Py_PakFile *self)
 static PyObject *
 PakFile_open(Py_PakFile *self, PyObject *args)
 {
-    size_t fileSize;
+    Py_ssize_t size;
     char *fileName = NULL;
-    char *fileString = NULL;
-    PyObject *pyFile = NULL;
-    if (!PyArg_ParseTuple(args, "s#", &fileName, &fileSize))
+    if (!PyArg_ParseTuple(args, "s#", &fileName, &size))
 	return NULL;
 
     self->pakFile->open(fileName);
-    return pyFile;
+    self->fileSize = getStreamSize(*self->pakFile);
+    self->mode = MODE_READ;
+    return Py_True;
+}
+
+static PyObject *
+PakFile_read(Py_PakFile *self, PyObject *args)
+{
+	std::streamoff	offset = static_cast<std::streamoff>(self->pakFile->tellg());
+	size_t bytesrequested = -1,
+	       left = self->fileSize - offset;
+	uint8_t *buffer = NULL;
+	PyObject *v;
+
+	if (!PyArg_ParseTuple(args, "|l:read", &bytesrequested))
+		return NULL;
+
+	if (bytesrequested > left)
+		bytesrequested = left;
+	if (bytesrequested > UINT_MAX) {
+		PyErr_SetString(PyExc_OverflowError,
+	"requested number of bytes is more than a Python string can hold");
+		return NULL;
+	}
+	buffer = (uint8_t*)malloc(bytesrequested);
+	self->pakFile->read((char*)buffer, bytesrequested);
+	if(static_cast<std::streamoff>(self->pakFile->tellg())  == self->fileSize-1)
+	    self->mode = MODE_READ_EOF;
+
+	v = PyString_FromStringAndSize((char *)buffer, bytesrequested);
+	if (v == NULL)
+		return NULL;
+	return v;
+}
+
+static PyObject *
+PakFile_seek(Py_PakFile *self, PyObject *args)
+{
+    int where = 0;
+    PyObject *offobj;
+    std::streamoff offset;
+    PyObject *ret = NULL;
+
+    if (!PyArg_ParseTuple(args, "O|i:seek", &offobj, &where))
+	return NULL;
+
+    switch (self->mode) {
+	case MODE_READ:
+	    self->pakFile->seekg(where, std::ios::beg);
+	    self->pakFile->seekg(PyInt_AsLong(offobj), std::ios::cur);
+	    offset = static_cast<std::streamoff>(self->pakFile->tellg());
+	    if(static_cast<std::streamoff>(self->pakFile->tellg())  == self->fileSize-1)
+		self->mode = MODE_READ_EOF;
+
+	case MODE_READ_EOF:
+	    break;
+
+	case MODE_CLOSED:
+	    PyErr_SetString(PyExc_ValueError,
+		    "I/O operation on closed file");
+	    goto cleanup;
+
+	default:
+	    PyErr_SetString(PyExc_IOError,
+		    "seek works only while reading");
+	    goto cleanup;
+    }	
+
+    Py_INCREF(Py_None);
+    ret = Py_None;
+
+cleanup:
+    return ret;
 }
 
 static PyMethodDef PakFile_methods[] = {
 	{"listfiles", (PyCFunction)PakFile_listfiles, METH_NOARGS, NULL},
 	{"open", (PyCFunction)PakFile_open, METH_VARARGS, NULL},
+	{"read", (PyCFunction)PakFile_read, METH_VARARGS, NULL},
+	{"seek", (PyCFunction)PakFile_seek, METH_VARARGS, NULL},
 	{NULL, NULL, 0, NULL}		/* sentinel */
 };
 
