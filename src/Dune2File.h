b24d86a199c7db122edc9a8dbfa311a45521399f
Per Øyvind Karlsen <proyvind@gmail.com>
2009-09-07 10:39:26 +0200

    adopt segra's data structure in stead

--- src/Dune2File.h
+++ src/Dune2File.h
@@ -11,7 +11,7 @@
 
 namespace eastwood {
 
-enum Dune2Version {
+enum	Dune2Version {
     D2_DEMO,
     D2_V1_00,
     D2_V1_07_US,
@@ -20,148 +20,110 @@ enum Dune2Version {
     D2_V1_00_RU, // TODO
     D2_VERSIONS
 };
-off_t versionOffset[D2_VERSIONS] = { 225278, 229282, 228274, 229682, 229586, 0 };
+off_t	versionOffset[D2_VERSIONS] = { 225278, 229282, 228274, 229682, 229586, 0 };
 
-off_t buildingOffset[D2_VERSIONS] = { 199930, 196570, 193930, 194010, 193930, 0 };
-struct buildingType {
-    uint16_t	shortNameID;
-    uint16_t	nameCodeRef;
-    uint32_t	longNameID;
-    uint32_t	wsaFileCodeRef;
-    uint8_t	u005b1			:1,
-		constructionScreen	:1,
-		u005b3			:1,
-		noConreteRequired	:1,
-		u005b4			:1,
-		u005b5			:1,
-		u005b6			:1,
-		canBeCaptured		:1;
-    uint8_t	u005b;
-    uint16_t	infantrySpawnCells;
+off_t	buildingOffset[D2_VERSIONS] = { 199930, 196570, 193930, 194010, 193930, 0 };
+struct	structureData {
+    uint16_t	idShort;
+    uint32_t	name;
+    uint16_t	idLong;
+    uint32_t	picture;
+    uint16_t	buildOpts;
+    uint16_t	field_E;
     uint16_t	hitPoints;
-    uint16_t	sight;
-    uint16_t	sidebarIconGfxID;
+    uint16_t	sight;    
+    uint16_t	sidebarIconID;
     uint16_t	cost;
     uint16_t	buildTime;
-    uint16_t	techLevel;
-    uint32_t	prerequisities;
+    uint16_t	techlevel;
+    uint32_t	preReqs;
     uint8_t	buildOrder;
-    uint8_t	cyUpgradesNeeded;
-    uint8_t	u016;
-    uint8_t	u017;
-    uint8_t	u018;
-    uint8_t	u019;
-    uint8_t	u020;
-    uint8_t	u021;
-    uint8_t	u022;
-    uint8_t	u023;
-    uint8_t	u024;
+    uint8_t	cYUpgradesNeeded;
+    uint8_t	field_22;
+    uint8_t	field_23;
+    uint8_t	field_24;
+    uint8_t	field_25;
+    uint8_t	field_26;
+    uint8_t	field_27;
+    uint8_t	field_28;
+    uint8_t	field_29;
+    uint8_t	field_2A;
     uint16_t	structureID;
     uint16_t	weaponDamage;
-    uint16_t	threatLevelForAI;
-    uint32_t	owner;
+    uint16_t	weaponDamageAdd;
+    uint8_t	owner;
     uint16_t	repairFacility;
     uint16_t	unitsCanEnter;
     uint16_t	spiceStorage;
-    uint16_t	powerConsumed;
+    uint16_t	powerUsage;
     uint16_t	foundationSize;
     uint16_t	structureGfxID;
-    uint16_t	u035;
-    uint16_t	u036;
-    uint16_t	u037;
-    uint16_t	u038;
-    uint16_t	u039;
-    uint16_t	u040;
-    uint16_t	constructionOptn1;
-    uint16_t	constructionOptn2;
-    uint16_t	constructionOptn3;
-    uint16_t	constructionOptn4;
-    uint16_t	constructionOptn5;
-    uint16_t	constructionOptn6;
-    uint16_t	constructionOptn7;
-    uint16_t	constructionOptn8;
-    uint16_t	upgradeTechLvl1;	
-    uint16_t	upgradeTechLvl2;	
-    uint16_t	upgradeTechLvl3;	
+    uint16_t	field_3E;
+    uint16_t	field_40;
+    uint16_t	field_42;
+    uint16_t	field_44;
+    uint16_t	field_46;
+    uint16_t	field_48;
+    uint16_t	constructOpt1;
+    uint16_t	constructOpt2;
+    uint16_t	constructOpt3;
+    uint16_t	constructOpt4;
+    uint16_t	constructOpt5;
+    uint16_t	constructOpt6;
+    uint16_t	constructOpt7;
+    uint16_t	constructOpt8;
+    uint16_t	techUpgrade1;
+    uint16_t	techUpgrade2;
+    uint16_t	techUpgrade3;
 } __attribute__ ((packed));
 
-off_t unitOffset[D2_VERSIONS] = { 201840, 198480, 195760, 195840, 195760, 0 };
-struct unitType {
-    uint16_t	shortNameID;
-    uint16_t	nameCodeRef;
-    uint32_t	longNameID;
-    uint32_t	wsaFileCodeRef;
-    uint8_t	airUnitShadow		:1,
-		u005b2			:1,
-		u005b3			:1,
-		u005b4			:1,
-		u005b5			:1,
-		wormCamouflage		:1,
-		turretRotates		:1,
-		u005b8			:1;
-    uint8_t	pickupForRepairs	:1,
-		u006b2			:1,
-		u006b3			:1,
-		u006b4			:1,
-		u006b5			:1,
-		u006b6			:1,
-		u006b7			:1,
-		u006b8			:1;
-    uint16_t	u007a;
-    uint16_t	hitPoints;
-    uint16_t	sight;
-    uint16_t	sidebarIconGfxID;
-    uint16_t	cost;
-    uint16_t	buildTime;
-    uint16_t	techLevel;
-    uint32_t	prerequisities;
-    uint8_t	u015;
-    uint8_t	upgradesNeeded;
-    uint16_t	sidebarCommand1;
-    uint16_t	sidebarCommand2;
-    uint16_t	sidebarCommand3;
-    uint16_t	sidebarCommand4;
-    uint16_t	u021;
-    uint8_t	u022;
-    uint16_t	u023;
-    uint16_t	threatLevelForAI;
-    uint32_t	owner;
-    uint16_t	unitArrayRangeMin;
-    uint16_t	unitArrayRangeMax;
-    uint8_t	u028			:1,
-		explodeOnTarget		:1,
-		explodeWhenDying	:1,
-		sonicImmunity		:1,
-		bumpyMovement		:1,
-		trackedCrushing		:1,
-		hasUnderControl		:1,
-		stayOnMap		:1;
-    uint8_t	u029b1			:1,
-		u029b2			:1,
-		firesTwice		:1,
-		u029b4			:1,
-		u029b5			:1,
-		u029b6			:1,
-		u029b7			:1,
-		notAWeaponType		:1;
-    uint8_t	u030;
-    uint8_t	u031;
-    uint16_t	u032;
-    uint16_t	movementType;
-    uint16_t	u034;
-    uint16_t	speed;
-    uint16_t	turningSpeed;
-    uint16_t	unitGfxID;
-    uint16_t	turretGfxID;
-    uint16_t	u039;
-    uint16_t	u040;
-    uint16_t	deathAnimation;
-    uint16_t	weaponFireRate;
-    uint16_t	weaponRange;
-    uint16_t	weaponDamage;
-    uint16_t	u045;
-    uint16_t	weaponType;
-    uint16_t	weaponSound;
+off_t	unitOffset[D2_VERSIONS] = { 201840, 198480, 195760, 195840, 195760, 0 };
+struct	unitData {
+   uint16_t	stringID;        
+   uint32_t	name;
+   uint16_t	stringLongID;    
+   uint32_t	picture;
+   uint16_t	optsWocaTuro;    
+   uint16_t	field_E;        
+   uint16_t	hitPoints;      
+   uint16_t	sight;          
+   uint16_t	sidebarIconID;  
+   uint16_t	cost;            
+   uint16_t	buildTime;      
+   uint16_t	techLevel;      
+   uint32_t	preReqs;        
+   uint8_t	field_20;        
+   uint8_t	upgradesNeeded;  
+   uint16_t	sidebarCommand1;
+   uint16_t	sidebarCommand2;
+   uint16_t	sidebarCommand3;
+   uint16_t	sidebarCommand4;
+   uint8_t	field_2A;        
+   uint16_t	field_2B;        
+   uint16_t	field_2D;        
+   uint16_t	aggressivity;    
+   uint8_t	owner;          
+   uint16_t	indexMin;        
+   uint16_t	indexMax;        
+   uint16_t	optsFitW;        
+   uint8_t	field_38;        
+   uint8_t	field_39;        
+   uint16_t	field_3A;        
+   uint16_t	movementType;    
+   uint16_t	field_3E;        
+   uint16_t	speed;
+   uint16_t	turningSpeed;
+   uint16_t	unitGfxID;
+   uint16_t	turretGfxID;
+   uint16_t	field_48;
+   uint16_t	field_4A;
+   uint16_t	deathAnim;
+   uint16_t	weaponRateFire;
+   uint16_t	weaponRange;
+   uint16_t	weaponDamage;
+   uint16_t	field_54;  
+   uint16_t	weaponType;
+   uint16_t	weaponSound;
 } __attribute__ ((packed));
 
 class Dune2File
