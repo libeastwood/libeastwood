49f012b32c717cb2b27266ce24c96add9e55a951
Per Øyvind Karlsen <proyvind@gmail.com>
2009-09-09 23:49:15 +0200

    indent

--- src/Exception.h
+++ src/Exception.h
@@ -13,47 +13,48 @@ enum logLevel
     LOG_WARNING,
     LOG_INFO
 };
-    
-    class Exception {
-        public:
-            Exception(logLevel level, std::string location, std::string message);
-            virtual ~Exception(){}
-            inline logLevel getLogLevel() { return m_level; }
-            inline std::string getLocation() { return m_location; }
-            inline std::string getMessage() { return m_message; }
 
-        protected:
-            logLevel m_level;
-            std::string m_location;
-            std::string m_message;
-    };
+class Exception {
+    public:
+        Exception(logLevel level, std::string location, std::string message);
+        virtual ~Exception(){}
+        inline logLevel getLogLevel() { return m_level; }
+        inline std::string getLocation() { return m_location; }
+        inline std::string getMessage() { return m_message; }
 
-    class FileException : public Exception {
-        public:
-            FileException(logLevel level, std::string location, std::string filename, std::string message);
-            inline std::string getFilename() { return m_filename; };
+    protected:
+        logLevel m_level;
+        std::string m_location;
+        std::string m_message;
+};
 
-            inline std::string getMessage() {
-                std::stringstream ss;
-                ss << m_filename << ": " << m_message;
-                return ss.str();
-            }
+class FileException : public Exception {
+    public:
+        FileException(logLevel level, std::string location, std::string filename, std::string message);
+        inline std::string getFilename() { return m_filename; };
 
-        private:
-            std::string m_filename;
+        inline std::string getMessage() {
+            std::stringstream ss;
+            ss << m_filename << ": " << m_message;
+            return ss.str();
+        }
+
+    private:
+        std::string m_filename;
 
 
-    };
+};
 
-    class FileNotFoundException : public FileException {
-        public:
-            FileNotFoundException(logLevel level, std::string location, std::string filename);
-    };
+class FileNotFoundException : public FileException {
+    public:
+        FileNotFoundException(logLevel level, std::string location, std::string filename);
+};
 
-    class NullSizeException : public Exception {
-        public:
-            NullSizeException(logLevel level, std::string location, std::string message);
-    };
+class NullSizeException : public Exception {
+    public:
+        NullSizeException(logLevel level, std::string location, std::string message);
+};
+
 }
 #endif // EASTWOOD_EASTWOODEXCEPTION_H
 
