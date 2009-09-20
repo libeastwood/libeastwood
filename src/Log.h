0e8afc75b878e4ad23eef3c15bfe4a8b11037685
Per Øyvind Karlsen <proyvind@gmail.com>
2009-09-20 21:53:32 +0200

    remove unused macros

--- src/Log.h
+++ src/Log.h
@@ -1,12 +1,9 @@
 #ifndef EASTWOOD_LOG_H
 #define EASTWOOD_LOG_H
 
-#define LOG(...)           ((void)(0))
 #define LOG_FATAL(...)     ((void)(0))
 #define LOG_ERROR(...)     ((void)(0))
 #define LOG_WARNING(...)   ((void)(0))
 #define LOG_INFO(...)      ((void)(0))
 
-#define LOG_INDENT(...)      ((void)(0))
-#define LOG_UNINDENT(...)    ((void)(0))
 #endif // EASTWOOD_LOG_H
