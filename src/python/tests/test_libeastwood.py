cec026ebc456e5f4a1b3338296bdf2869f100835
Per Øyvind Karlsen <proyvind@gmail.com>
2009-10-01 19:25:46 +0200

    fix typo

--- src/python/tests/test_libeastwood.py
+++ src/python/tests/test_libeastwood.py
@@ -6,7 +6,7 @@
 # Copyright (c) 2009 Per Øyvind Karlsen <peroyvind@mandriva.org>
 #
 # This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public # License as published
+# modify it under the terms of the GNU General Public License as published
 # by the Free Software Foundation; either version 3 of the License,
 # or (at your option) any later version.
 #
