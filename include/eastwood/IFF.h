415cc95f0d71227df85b98b487c7b0853afbea01
Per Øyvind Karlsen <proyvind@gmail.com>
2009-10-21 04:12:03 +0200

    add some more IFF chunk identifiers

--- include/eastwood/IFF.h
+++ include/eastwood/IFF.h
@@ -10,12 +10,24 @@
 #endif
 
 enum IFF_ID {
+    ID_CAT  = MKID_BE('C','A','T',' '),
+    ID_DATA = MKID_BE('D','A','T','A'),
+    ID_DESC = MKID_BE('D','E','S','C'),
+    ID_EMC2 = MKID_BE('E','M','C','2'),
+    ID_EVNT = MKID_BE('E','V','N','T'),
     ID_FORM = MKID_BE('F','O','R','M'),
     ID_ICON = MKID_BE('I','C','O','N'),
+    ID_INFO = MKID_BE('I','N','F','O'),
+    ID_MENT = MKID_BE('M','E','N','T'),
+    ID_NAME = MKID_BE('N','A','M','E'),
+    ID_ORDR = MKID_BE('O','R','D','R'),
     ID_RTBL = MKID_BE('R','T','B','L'),
     ID_RPAL = MKID_BE('R','P','A','L'),
     ID_SSET = MKID_BE('S','S','E','T'),
     ID_SINF = MKID_BE('S','I','N','F'),
+    ID_TIMB = MKID_BE('T','I','M','B'),    
+    ID_XDIR = MKID_BE('X','D','I','R'),    
+    ID_XMID = MKID_BE('X','M','I','D'),
     ID_FILLER = 0
 };
 #endif // EASTWOOD_IFF_H
