c2c6a7f99ad6f74e822d0ee90aa85def339ff06a
omniblade <nadia_xy@hotmail.com>
2014-10-12 00:41:08 +0100

    Implementd archive manager untested

---
+++ include/eastwood/ArchiveManager.h
@@ -0,0 +1,38 @@
+#ifndef EASTWOOD_ARCHIVEMANAGER_H
+#define	EASTWOOD_ARCHIVEMANAGER_H
+
+#include "StdDef.h"
+#include "ArcStream.h"
+#include "DirEnt.h"
+#include <vector>
+#include <map>
+
+typedef std::map<int32_t, ArcFileInfo> t_arc_index;
+typedef std::pair<int32_t, ArcFileInfo> t_arc_entry;
+typedef std::map<int32_t, ArcFileInfo>::iterator t_arc_index_iter;
+
+namespace eastwood {
+
+class ArchiveManager
+{
+public:
+    ArchiveManager() : _archives(0), _stream() {}
+    size_t indexDir(std::string& path);
+    size_t indexPak(std::string& pakfile, bool usefind = false);
+    size_t indexMix(std::string& mixfile, bool usefind = false);
+    ArcFileInfo& find(std::string filename);
+    int size() { return _archives.size(); }
+    bool empty() { return _archives.empty(); }
+    
+private:
+    int32_t idGen(std::string filename);
+    void handleEncrypted(ArcFileInfo& archive);
+    void handleUnEncrypted(ArcFileInfo& archive, uint16_t filecount);
+    std::vector<t_arc_index> _archives;
+    ArcIOStream _stream;
+};
+
+}//eastwood
+
+#endif	/* EASTWOOD_ARCHIVEMANAGER_H */
+
