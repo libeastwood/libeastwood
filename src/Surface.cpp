72d3180e9ad34e1440dded98704ccbf53a3f6a33
Per Øyvind Karlsen (proyvind) <peroyvind@mandriva.org>
2014-12-24 07:10:16 +0100

    fix "warning: control may reach end of non-void function" in assignment operator

--- src/Surface.cpp
+++ src/Surface.cpp
@@ -56,8 +56,7 @@ Surface::Surface(const Surface &surface) :
     _bpp(surface._bpp), _Bpp(surface._Bpp), _width(surface._width), _height(surface._height),
     _pitch(surface._pitch), _pixels(new Bytes(new uint8_t[surface.size()])), _palette(surface._palette)
 {
-    if(*this)
-    	memcpy(*this, surface, size());
+    return *this;
 }
 
 Surface::~Surface() {
