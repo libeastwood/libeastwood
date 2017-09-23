5fe2feb619c935e905e17f6381f37d712bea4659
Per Øyvind Karlsen <proyvind@moondrake.org>
2017-09-23 20:59:00 +0200

    update mail & GPL version in top disclaimer

--- python/setup.py
+++ python/setup.py
@@ -3,12 +3,12 @@
 #
 # Python Bindings for libeastwood
 #
-# Copyright (c) 2009 Per Øyvind Karlsen <peroyvind@mandriva.org>
+# Copyright (c) 2009 Per Øyvind Karlsen <proyvind@moondrake.org>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation; either
-# version 3 of the License, or (at your option) any later version.
+# version 2 of the License, or (at your option) any later version.
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
